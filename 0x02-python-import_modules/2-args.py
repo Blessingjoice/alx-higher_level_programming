8ebbeac8697b5ecf72a60e499d6228781ae3f4f6
Blessingjoice <blessingnwabuoku@gmail.com>
2023-12-02 14:22:38 -0800

    oneday

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the number of and list of arguments"""
+    import sys
+
+    num = len(sys.argv) - 1
+    if num == 0:
+        print("0 arguments")
+    elif num == 1:
+        print("1 argument")
+    else:
+        print("{} argument:".format(num))
+    for i in range(num):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
