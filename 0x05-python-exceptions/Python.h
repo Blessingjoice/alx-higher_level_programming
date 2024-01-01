f60aefa7a7cd29e518adf6232cc365ee3369cb64
Blessingjoice <blessingnwabuoku@gmail.com>
2024-01-01 02:11:57 -0800

    a new yar

---
+++ 0x05-python-exceptions/Python.h
@@ -0,0 +1,10 @@
+ifndef PYTHON_H
+#define PYTHON_H
+
+#include <stdlib.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif /* PYTHON_H */
